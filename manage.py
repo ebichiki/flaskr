084cd823450a98694967ff7a9726e3871d927aeb
ebichiki <allegro.con.spirito28@gmail.com>
2018-12-18 22:40:53 +0900

    login form

--- manage.py
+++ manage.py
@@ -1,2 +1,26 @@
+import argparse
 from flaskr import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+from flaskr.models import init
+
+
+def run():
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
+
+def init_db():
+    init()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="subcommand")
+    subparsers.required = True
+
+    foo_parser = subparsers.add_parser("run")
+    foo_parser.set_defaults(fn=run)
+
+    bar_parser = subparsers.add_parser("init_db")
+    bar_parser.set_defaults(fn=init_db)
+
+    args = parser.parse_args()
+    args.fn()
